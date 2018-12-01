4cec36756f5220c3665996d5239c80ce751cf390
Nik Trajic <ntrajic@1090114bclt.onmicrosoft.com>
2018-11-30 19:24:07 -0800

    Fully implement main Parse() func:
    import ("strings")
    func Parse(line string) string {
    	return strings.Split(line, ";")[0]
    }

--- runes.go
+++ runes.go
@@ -1,13 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import ("fmt")
+import ("strings")
 
 func main() {
 	fmt.Println("Please provide one or more words to search.")
 }
 
 func Parse(line string) string {
-	return "0041"
+	return strings.Split(line, ";")[0]
 }
