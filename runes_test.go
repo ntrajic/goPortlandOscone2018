cd8f407a5f63909a9690a66806b841fa42830e7f
Nik Trajic <ntrajic@1090114bclt.onmicrosoft.com>
2018-11-30 18:48:57 -0800

    Terminal Window: C:\SRC\GO\runescan\runes000>go test # _/C_/SRC/GO/runescan/runes000 [_/C_/SRC/GO/runescan/runes000.test] .\runes_test.go:9:19: undefined: testing .\runes_test.go:13:9: undefined: Parse FAIL    _/C_/SRC/GO/runescan/runes000 [build failed]

--- runes_test.go
+++ runes_test.go
@@ -5,3 +5,17 @@ func Example () {
 	// Oputput:
 	// Please provide one or more words to search.
 }
+
+func TestParse(t *testing.T) {
+	// Given
+	const line= "0041;LATIN CAPITAL LETTER A;Lu;0;L;;;;;N;;;;0061;"
+	// When
+	got := Parse(line)
+	// Then
+	want := "0041"
+	if want != got {
+		t.Errorf("got: %v, want: %v", got, want)
+	}
+
+
+}
